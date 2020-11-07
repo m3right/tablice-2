6cadbe05f5ce95c28d7b4096bbc216cc8f76c7ef
Rafał Dreżewski <drezew@agh.edu.pl>
2020-11-07 19:40:14 +0100

    init

---
+++ Main.java
@@ -0,0 +1,45 @@
+import java.io.*;
+
+public class Main {
+
+   public static int suma(int[] tablica, int rozmiar) {
+      int s = 0;
+      for ( int i = 0; i < rozmiar; i++ )
+         s += tablica[i];
+      return s;
+   }
+
+   public static int minimum(int[] tablica, int rozmiar) {
+      int min = tablica[0];
+      for ( int i = 1; i < rozmiar; i++ )
+         if ( tablica[i] < min )
+            min = tablica[i];
+      return min;
+   }
+   
+   public static void wczytaj_tablice(int[] tablica, int rozmiar) throws IOException {
+      BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+      for ( int i = 0; i < rozmiar; i++ ) {
+         System.out.println("Podaj liczbe: ");
+         tablica[i] = Integer.parseInt(in.readLine());
+      }
+   }
+   
+   public static void wypisz_tablice(int[] tablica, int rozmiar) {
+      for ( int i = 0; i < rozmiar; i++ )
+         System.out.println("tablica["+i+"] = "+tablica[i]);
+   }
+
+   public static void main(String[] args) {
+      
+   }
+}
+
+/*
+ * Cwiczenia:
+ * 1. Z pomoca zaproponowanych funkcji napisz programy podajace 
+ * odpowiednio sume i minimum podanych przez uzytkownika (lub wygenerowanych) liczb.
+ * 2. Napisz funkcje obliczajaca srednia dla podanego (lub 
+ * wygenerowanego) ciagu liczb.
+ */
+ 
